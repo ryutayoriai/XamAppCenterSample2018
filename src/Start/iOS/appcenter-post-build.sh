e6a9c040dc4548b8b7999653e852b72985b4dd2c
ryutayoriai <ryuta.y.082212@gmail.com>
2019-02-23 16:48:39 +0900

    post-build追加

---
+++ src/Start/iOS/appcenter-post-build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Post Build Script
+
+# Exit immediately if a command exits with a non-zero status (failure)
+set -e 
+
+##################################################
+# variables
+
+appCenterLoginApiToken=$AppCenterLoginToken # this comes from the build environment variables
+appName="TomohiroSuzuki128/XamAppCenterSample2018iOS" # 自分のアプリ名に書き換える
+deviceSetName="TomohiroSuzuki128/my-devices" # 自分のデバイスセット名に書き換える
+publishedAppFileName="XamAppCenterSample2018.iOS.ipa"
+sourceFileRootDir="$APPCENTER_SOURCE_DIRECTORY/src/Start"
+uiTestProjectName="UITests"
+testSeriesName="all-tests"
+##################################################
+
+echo "##################################################################################################"
+echo "Post Build Script"
+echo "##################################################################################################"
+echo "Starting Xamarin.UITest"
+echo "   App Name: $appName"
+echo " Device Set: $deviceSetName"
+echo "Test Series: $testSeriesName"
+echo "##################################################################################################"
+echo ""
+
+echo "> Build UI test projects"
+find $sourceFileRootDir -regex '.*Test.*\.csproj' -exec msbuild {} \;
+
+echo "> Run UI test command"
+# Note: must put a space after each parameter/value pair
+appcenter test run uitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/$publishedAppFileName --test-series $testSeriesName --locale "ja_JP" --build-dir $sourceFileRootDir/$uiTestProjectName/bin/Debug --uitest-tools-dir $sourceFileRootDir/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken 
+
+echo ""
+echo "##################################################################################################"
+echo "Post Build Script complete"
+echo "##################################################################################################"
