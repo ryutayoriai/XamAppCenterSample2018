a82fd85cbd19548eb5051405b742f2e82996576a
ryutayoriai <ryuta.y.082212@gmail.com>
2019-02-23 17:51:02 +0900

    ユーザ名変更

--- src/Start/iOS/appcenter-post-build.sh
+++ src/Start/iOS/appcenter-post-build.sh
@@ -9,8 +9,8 @@ set -e
 # variables
 
 appCenterLoginApiToken=$AppCenterLoginToken # this comes from the build environment variables
-appName="ryutayoriai/XamAppCenterSample2018iOS" # 自分のアプリ名に書き換える
-deviceSetName="ryutayoriai/my-devices" # 自分のデバイスセット名に書き換える
+appName="ryuta-y-hotmail.co.jp/XamAppCenterSample2018iOS" # 自分のアプリ名に書き換える
+deviceSetName="ryuta-y-hotmail.co.jp/my-devices" # 自分のデバイスセット名に書き換える
 publishedAppFileName="XamAppCenterSample2018.iOS.ipa"
 sourceFileRootDir="$APPCENTER_SOURCE_DIRECTORY/src/Start"
 uiTestProjectName="UITests"
